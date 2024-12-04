b08e756c61e40d3c7afe949ec10bb4fc0d31460a
Medine Hemidova <maddyhamidova2010@gmail.com>
2024-12-04 13:02:44 +0400

    for

---
+++ app.js
@@ -0,0 +1,60 @@
+// const n = +prompt("ededi daxil et");
+// let cem = 0;
+// for(let i = 1; i < n; i++){
+//     cem += i;
+// }
+// document.write(`1den ${n} ededine qeder butun reqemlerin cemi = ${cem}`)
+
+// const rows = 4;
+// for(let i = rows; i > 0; i--){
+//     document.write("*" .repeat(i) + "<br/>")
+// }
+
+// const n = 5;
+// let hasil = 1;
+// for(let i = 1; i <= n; i++){
+//     hasil *= i;
+// }
+// document.write(`1den ${n} ededine qeder butun reqemlerin hasili = ${hasil}`)
+
+// const n = 22;
+// for(let i = n ; i <= 106; i += 7 ){
+//     console.log(i);
+// }
+
+// const n = 5;
+// let i =1;
+// while (i <= n){
+//     i++;
+//     console.log("I know how to use cycles");
+// }
+
+// for(let i = 100; i <= 990; i++){
+//     if(!(i % 10)){
+//         console.log(i);
+//     }
+// }
+
+// let cem = 0;
+// for(let i = 11; i <= 99; i++){
+//     if(i % 2 === 1){
+//         cem += i;
+//     }
+// }
+// console.log(cem);
+
+// const n = 14;
+// for(let i = 100; i <= 999; i++){
+//     if(!(i % n)){
+//         console.log(i);
+        
+//     }
+// }
+
+// const n = 16
+// for(let i = n; i > 0; i--){
+//     if(i % 2){
+//         console.log(i);
+        
+//     }
+// }
